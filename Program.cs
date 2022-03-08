375e54b53e124c575c0fd714f5bed82503600f5f
Vladyslav Zhukov <epicmand.mi@gmail.com>
2022-03-08 17:38:45 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino.Mocks;
+using 
+
+namespace AlgorithmsLab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
