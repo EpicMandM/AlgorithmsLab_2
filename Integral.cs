9909ec7400b6fce7eb336fa4e5859fe266966025
Vladyslav Zhukov <epicmand.mi@gmail.com>
2022-03-08 19:44:31 +0200

    Square integration implemented

--- Integral.cs
+++ Integral.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace AlgorithmsLab_2
 {
-    class Integral
+    static class Integral
     {
+        internal static double LeftSquare(Func<double, double> func, double a, double b, double n)
+        {
+            double h = (b - a) / n;
+            double sum = 0;
+            for (int i = 0; i <= n-1; i++)
+            {
+                sum += func(a + i * h);
+            }
+            return h*sum;
+
+        }
+
+        internal static double RightSquare(Func<double, double> func, double a, double b, double n)
+        {
+            double h = (b - a) / n;
+            double sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum += func(a + i * h);
+            }
+            return h * sum;
+        }
+
+        internal static double MiddleSquare(Func<double, double> func, double a, double b, double n)
+        {
+            double h = (b - a) / n;
+            double sum = (func(a) + func(b)) /2;
+            for (int i = 1; i <= n-1; i++)
+            {
+                sum += func(a + i * h);
+            }
+            return h * sum;
+        }
     }
 }
