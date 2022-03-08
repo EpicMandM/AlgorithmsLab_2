913312c4f0eb2d23d997f9eda62efb6b7fe8907b
Vladyslav Zhukov <epicmand.mi@gmail.com>
2022-03-08 19:27:36 +0200

    Added print method

--- Program.cs
+++ Program.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Rhino.Mocks;
-using 
-
 namespace AlgorithmsLab_2
 {
     class Program
     {
+        static void Print(double[] x, double[] l, double[] m, double[] r)
+        {
+            Console.WriteLine("|-------|-------|-------|-------|-------|");
+            Console.WriteLine("|   i   |   x   |  l_i  |  m_i  |  r_i  |");
+            Console.WriteLine("|-------|-------|-------|-------|-------|");
+            for (long i = 0; i < x.LongLength; i++)
+            {
+                Console.Write("|");
+                Console.Write("{0,-7:F2}|", i);
+                Console.Write(x[i] < 0 ? "{0,-7:F2}|" : " {0,-6:F2}|", x[i]);
+                Console.Write(l[i] < 0 ? "{0,-7:F2}|" : " {0,-6:F2}|", l[i]);
+                Console.Write(m[i] < 0 ? "{0,-7:F2}|" : " {0,-6:F2}|", m[i]);
+                Console.Write(r[i] < 0 ? "{0,-7:F2}|" : " {0,-6:F2}|", r[i]);
+                Console.WriteLine();
+            }
+            Console.WriteLine("|-------|-------|-------|-------|");
+        }
         static void Main(string[] args)
         {
         }
