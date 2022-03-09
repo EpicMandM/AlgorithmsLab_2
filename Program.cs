66e541661a4e98b68ac4aefabadc4d40d7f0844c
Vladyslav Zhukov <epicmand.mi@gmail.com>
2022-03-09 14:40:11 +0200

    Runge rule for estimating errors implemented

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace AlgorithmsLab_2
     class Program
     {
         static Func<double, double> func = t => t != 1 ? Math.Log(t) / (1 - Math.Pow(t, 2)) : 0;
+        const double bottomBorder = 0.1;
+        const double N = 10;
         static void Print(double[] x, double[] l, double[] m, double[] r)
         {
             Console.WriteLine("|-------|-------|-------|-------|-------|");
@@ -25,7 +27,7 @@ namespace AlgorithmsLab_2
             }
             Console.WriteLine("|-------|-------|-------|-------|-------|");
         }
-        static ICollection<double> xs;
+        private static ICollection<double> xs;
         static Program()
         {
             xs = GetXs();
@@ -33,26 +35,47 @@ namespace AlgorithmsLab_2
         static ICollection<double> GetXs()
         {
             List<double> xs = new List<double>();
-            for (double i = 0.1; i <= 1; i = i + 0.1)
+            for (double i = bottomBorder; i <= 1; i = i + bottomBorder)
             {
                 xs.Add(i);
             }
             return xs;
         }
-        static double[] GetIntergal(ICollection<double> xs, IIntegral integral)
+        static double[] GetIntergal(IIntegral integral)
+        {
+            return GetIntegral(integral, N); 
+        }
+        static double[] GetIntegral(IIntegral integral, double h)
         {
             List<double> integ = new List<double>();
             foreach (double item in xs)
             {
-                integ.Add(integral.GetIntegral(func, 0.1, item, 10e4));
+                integ.Add(integral.GetIntegral(func, bottomBorder, item, h)); 
             }
             return integ.ToArray();
         }
-
+        static double DoRunge(IIntegral integral, double topBorder, double h, double double_h)
+        {
+            return Math.Abs(integral.GetIntegral(func, bottomBorder, topBorder, h) - integral.GetIntegral(func, bottomBorder, topBorder, double_h)) / 3;
+        }
+        static double[] GetRunge(IIntegral integral)
+        {
+            return GetRunge(integral, N);
+        }
+        static double[] GetRunge(IIntegral integral, double h)
+        {
+            List<double> runge = new List<double>();
+            foreach (double item in xs)
+            {
+                runge.Add(DoRunge(integral, item, h, h * 2));
+            }
+            return runge.ToArray();
+        }
         static void Main(string[] args)
         {
             ICollection<double> xs = GetXs();
-            Print(xs.ToArray(), GetIntergal(xs, new LeftIntegral()), GetIntergal(xs, new MiddleIntegral()), GetIntergal(xs, new RightIntegral()));
+            Print(xs.ToArray(), GetIntergal(new LeftIntegral()), GetIntergal(new MiddleIntegral()), GetIntergal(new RightIntegral()));
+            Print(xs.ToArray(), GetRunge(new LeftIntegral()), GetRunge(new MiddleIntegral()), GetRunge(new RightIntegral()));
         }
     }
 }
